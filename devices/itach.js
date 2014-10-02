ae201db5606ac245c9041a0cfb091338db75d9b2
Tom Atkinson <atkinson.tommy@nhome.ba>
2014-10-02 12:32:41 +0000

    Learning API initial version
    git-svn-id: svn://internal-repo/nsc/NHome/homeserver@97348 bc4cc02b-413e-4181-b44e-24092dc36e11

--- devices/itach.js
+++ devices/itach.js
@@ -4,6 +4,8 @@ var conn;
 
 var devices = {};
 
+var remotes = {};
+
 function log(msg)
 {
     console.log('[iTach]', msg);
@@ -16,6 +18,10 @@ module.exports = function(c) {
     conn.once('accepted', function (cfg) {
     
         log('Accepted');
+
+        if (cfg.itach_remotes) {
+            remotes = JSON.parse(cfg.itach_remotes);
+        }
 
         var d = new Itach.discovery();
 
@@ -51,6 +57,42 @@ function startListening()
     conn.on('IRLearn', function (info) {
         setLearningMode(info.id);
     });
+
+    conn.on('sendVolumeUp', function (remoteid) {
+        sendVolumeUp(remoteid);
+    });
+
+    conn.on('sendVolumeDown', function (remoteid) {
+        sendVolumeDown(remoteid);
+    });
+
+    conn.on('learnVolumeUp', function (remoteid) {
+        learnVolumeUp(remoteid);
+    });
+
+    conn.on('learnVolumeDown', function (remoteid) {
+        learnVolumeDown(remoteid);
+    });
+
+    conn.on('sendKey', function (remoteid, key) {
+        sendVolumeUp(remoteid, key);
+    });
+
+    conn.on('learnKey', function (remoteid, key) {
+        learnKey(remoteid, key);
+    });
+
+    conn.on('saveCustomRemote', function (remote) {
+        saveCustomRemote(remote);
+    });
+
+    conn.on('deleteCustomRemote', function (remoteid) {
+        deleteCustomRemote(remoteid);
+    });
+
+    conn.on('getCustomRemotes', function () {
+        getCustomRemotes();
+    });
 }
 
 function getRemotes()
@@ -78,9 +120,38 @@ function sendRawCommand(id, cmd)
     });
 }
 
-function setLearningMode(id)
+function sendVolumeUp(remoteid)
 {
-    console.log('setLearningMode');
+    sendKey(remoteid, 'VOLUME UP');
+}
+
+function sendVolumeDown(remoteid)
+{
+    sendKey(remoteid, 'VOLUME DOWN');
+}
+
+function learnVolumeUp(remoteid)
+{
+    learnKey(remoteid, 'VOLUME UP');
+}
+
+function learnVolumeDown(remoteid)
+{
+    learnKey(remoteid, 'VOLUME DOWN');
+}
+
+function sendKey(remoteid, key)
+{
+    var remote = remotes[remoteid];
+
+    cmd = remote.keys[key].replace('1:1', remote.connector);
+
+    sendRawCommand(remote.deviceid, cmd + '\r');
+}
+
+function learnKey(remoteid, key)
+{
+    var id = remotes[remoteid].deviceid;
 
     devices[id].dev.learn(function (err, res) {
 
@@ -89,6 +160,57 @@ function setLearningMode(id)
             return;
         }
 
-        conn.emit('IRCode', res);
+        saveCode(remoteid, key, res);
+    
+        conn.emit('IRKeyLearned', { remoteid: remoteid, key: key });
     });
+}
+
+function saveCode(remoteid, key, code)
+{
+    remotes[remoteid].keys[key] = code;
+
+    saveRemotes();
+}
+
+function saveRemotes()
+{
+    conn.emit('setConfig', { itach_remotes: JSON.stringify(remotes) });
+}
+
+function saveCustomRemote(r)
+{
+    r.keys = {};
+    r.id = require('node-uuid').v4();
+
+    remotes[r.id] = r;
+
+    saveRemotes();
+
+    conn.emit('customRemoteAdded', r);
+}
+
+function deleteCustomRemote(id)
+{
+    delete remotes[id];
+
+    saveRemotes();
+
+    conn.emit('customRemoteDeleted', id);
+}
+
+function getCustomRemotes()
+{
+    var customremotes = [], r;
+
+    for (r in remotes) {
+
+        var r2 = JSON.parse(JSON.stringify(remotes[r]));
+
+        r2.keys = Object.keys(r2);
+
+        customremotes.push(r2);
+    }
+
+    conn.emit('customRemotes', customremotes);
 }
