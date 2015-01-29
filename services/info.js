c2a9a82dd164ef8d9bbd268e69bd5fea396c1876
Tom Atkinson <atkinson.tommy@nhome.ba>
2015-01-29 12:40:19 +0000

    Update via web support
    git-svn-id: svn://internal-repo/nsc/NHome/homeserver@108659 bc4cc02b-413e-4181-b44e-24092dc36e11

--- services/info.js
+++ services/info.js
@@ -12,13 +12,18 @@ module.exports = function(c, l) {
     conn.on('getServerStatus', function (cb) {
        getServerStatus(cb);
     });
+
+    conn.on('updateApp', function() {
+        updateApp();        
+    });
 };
 
 function getServerStatus(cb)
 {
     var status = {
         ip: getIP(),
-        version: getVersion()
+        version: getVersion(),
+        updateable: getUpdateable()
     };
 
     conn.emit('serverStatus', status);
@@ -59,4 +64,51 @@ function getIP()
     }
 
     return addresses.join(', ');
+}
+
+function getUpdateable()
+{
+    if (process.env['NHOME_CAN_UPDATE'] === '1') {
+        return true;
+    }
+
+    if (process.platform === 'win32') {
+        return true;
+    }
+
+    return false;
+}
+
+function updateApp()
+{
+    // Our NHome pi image - systemd will update app and respawn us 
+    if (process.env['NHOME_CAN_UPDATE'] === '1') {
+        process.exit();
+        return;
+    }
+
+    // Windows - restarting app runs update.js again
+    if (process.platform === 'win32') {
+        reSpawnApp();
+        return;
+    }
+
+    logger.warn('Unable to handle updateApp request');
+}
+
+function reSpawnApp()
+{
+    var args = [].concat(process.argv);
+
+    args.shift();
+
+    var cp_opt = {
+        stdio: 'inherit',
+        cwd: process.cwd(),
+        detached: true
+    };
+
+    require('child_process').spawn(process.execPath, args, cp_opt);
+
+    process.exit();
 }
