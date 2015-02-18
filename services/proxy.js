b59e5329d4a01eee67235457015e5253d544ca47
Tom Atkinson <atkinson.tommy@nhome.ba>
2015-02-18 11:32:08 +0000

    Only use child processes on multicore machines
    git-svn-id: svn://internal-repo/nsc/NHome/homeserver@110920 bc4cc02b-413e-4181-b44e-24092dc36e11

--- services/proxy.js
+++ services/proxy.js
@@ -18,11 +18,47 @@ function proxyConnect(proxy)
 {
     logger.debug('Creating proxy to ' + proxy.host + ':' + proxy.port);
 
-    var child = require('child_process').fork(__dirname + '/proxy-child.js');
+    var numcores = require('os').cpus().length;
+    
+    if (numcores > 1) {
+    
+        var child = require('child_process').fork(__dirname + '/proxy-child.js');
 
-    child.on('message', function(message) {
-        logger.error(proxy.host + ':' + proxy.port, message);
-    });
+        child.on('message', function(message) {
+            logger.error(proxy.host + ':' + proxy.port, message);
+        });
 
-    child.send(proxy);
+        child.send(proxy);
+        
+    } else {
+    
+        var ext = require('net').connect(proxy.port, proxy.host, function() {
+        
+            ext.write(proxy.request);
+        
+            require('tls').connect({host: 'nhome.ba', servername: 'nhome.ba', port: 8082}, function() {
+
+                this.setNoDelay();
+
+                this.write(proxy.id);
+
+                ext.pipe(this).pipe(ext);
+        
+                this.on('error', function(err) {
+                    logger.error(proxy.host + ':' + proxy.port, err.message);
+                    ext.end();
+                    ext.destroy();
+                });
+        
+                this.on('close', function() {
+                    ext.end();
+                    ext.destroy();
+                });
+            });
+        });
+        
+        ext.on('error', function(err) {
+            logger.error(proxy.host + ':' + proxy.port, err.message);
+        });
+    }
 }
