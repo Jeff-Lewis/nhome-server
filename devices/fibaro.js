35745556e0e4138af0fc8546375eb2aa6d609b69
Tom Atkinson <atkinson.tommy@nhome.ba>
2015-06-11 13:05:14 +0000

    Revert: Attempt to support setValue2 blinds - does not work
    git-svn-id: svn://internal-repo/nsc/NHome/homeserver@121697 bc4cc02b-413e-4181-b44e-24092dc36e11

--- devices/fibaro.js
+++ devices/fibaro.js
@@ -338,13 +338,7 @@ function setShutterValue(id, value, cb)
 
     var deviceId = devices[id].id;
 
-    var action = 'setValue';
-
-    if (devices[id].type === 'com.fibaro.FGR222') {
-        action = 'setValue2';
-    }
-
-    devices[id].dev.call('callAction', { 'deviceID': deviceId, 'name': action, 'arg1': value }, function(err, result) {
+    devices[id].dev.call('callAction', { 'deviceID': deviceId, 'name': 'setValue', 'arg1': value }, function(err, result) {
 
         if (err) {
             log('setShutterValue:' + err);
