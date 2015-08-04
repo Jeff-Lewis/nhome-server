3406af066278133aa72d5e61ce35cee0be847bcb
Tom Atkinson <atkinson.tommy@nhome.ba>
2015-08-04 14:15:50 +0000

    Add support for running as a nwjs app in windows
    git-svn-id: svn://internal-repo/nsc/NHome/homeserver@125392 bc4cc02b-413e-4181-b44e-24092dc36e11

--- server.js
+++ server.js
@@ -16,50 +16,5 @@ program
 
 var log = require('./logger.js')(program);
 
-process.on('uncaughtException', function (err) {
-    log.error('uncaughtException:' + err);
-    log.error(err.stack);
-});
-
-log.debug('Loading configuration');
-
-var cfg = require('./configuration.js');
-
-cfg.load(log, function () {
-
-    var conn = require('./connection.js')(log);
-
-    log.debug('Loading services');
-
-    require('./services/namer.js').listen(conn, log);
-    require('./services/cats.js').listen(conn, log);
-    require('./services/schedule.js')(conn, log);
-    require('./services/proxy.js')(conn, log);
-    require('./services/info.js')(conn, log);
-    require('./services/streaming.js')(conn, log);
-    require('./services/cameras.js')(conn, log);
-    require('./services/scenes.js')(conn, log);
-    require('./services/weather.js')(conn, log);
-    require('./services/blacklist.js')(conn, log);
-    require('./services/remotes.js')(conn, log);
-
-    log.debug('Loading modules');
-
-    var modules = ['hue', 'wemo', 'insteon', 'itach', 'fibaro',
-        'razberry', 'lifx', 'netatmo', 'nhome', 'nest', 'nhomebridge',
-        'ecobee'
-    ];
-
-    var blacklist = cfg.get('blacklist_modules', []);
-
-    modules.filter(function (module) {
-        return blacklist.indexOf(module) === -1;
-    }).forEach(function (module) {
-        require('./devices/' + module + '.js')(conn, log);
-    });
-
-    log.info('Connecting...');
-
-    conn.connect();
-});
+require('./main.js')(log);
 
--- update.js
+++ update.js
@@ -1,107 +1,117 @@
 "use strict";
 
-var version = require('./package.json').version,
-    name = require('./package.json').name;
+module.exports = function (log, cb) {
 
-var https = require('https');
+    var version = require('./package.json').version,
+        name = require('./package.json').name;
 
-https.get('https://neosoft-updates.s3.amazonaws.com/zupdate/' + name + '/' + version + '.xml', function(res) {
+    var https = require('https');
 
-    console.log('Checking for updates');
+    https.get('https://neosoft-updates.s3.amazonaws.com/zupdate/' + name + '/' + version + '.xml', function(res) {
 
-    var updateXML = '';
+        log.info('Checking for updates');
 
-    res.on('data', function(d) {
-        updateXML += d;
-    });
-
-    res.on('end', function() {
-        if (res.statusCode === 200) {
-            require('xml2js').parseString(updateXML, processUpdateInfo);
-        } else {
-            console.error('Failed to download update info:', res.statusCode);
-            require('./server.js');
-        }
-    });
-
-}).on('error', function(e) {
-    console.error(e);
-    require('./server.js');
-});
-
-function processUpdateInfo(err, info)
-{
-    if (err) {
-        console.error('Failed to parse XML:', err);
-        require('./server.js');
-        return false;
-    }
-
-    if (!info.updates) {
-        console.log('Up to date');
-        require('./server.js');
-        return false;
-    }
-
-    var update = info.updates.update[0].patch[0].$;
-    update.size = parseInt(update.size, 10);
-
-    var crypto = require('crypto');
-
-    var zip = new Buffer(update.size);
-
-    var shasum = crypto.createHash(update.hashFunction);
-
-    https.get(update.URL, function(res) {
-
-        console.log('Downloading update');
-
-        var downloadedBytes = 0;
+        var updateXML = '';
 
         res.on('data', function(d) {
-            shasum.update(d);
-            d.copy(zip, downloadedBytes);
-            downloadedBytes += d.length;
+            updateXML += d;
         });
 
         res.on('end', function() {
-
-            if (downloadedBytes !== update.size) {
-                console.error('Download incomplete');
-                require('./server.js');
-                return;
+            if (res.statusCode === 200) {
+                require('xml2js').parseString(updateXML, processUpdateInfo);
+            } else {
+                log.error('Failed to download update info:', res.statusCode);
+                loaded();
             }
-
-            console.log('Download complete');
-
-            var checksum = shasum.digest('hex');
-
-            if (checksum !== update.hashValue) {
-                console.error('Checksum mismatch');
-                require('./server.js');
-                return;
-            }
-
-            console.log('Applying update');
-
-            var AdmZip = require('adm-zip');
-
-            var archive = new AdmZip(zip);
-
-            archive.extractAllTo('.', true);
-
-            console.log('Update complete');
-
-            require('./server.js');
         });
 
     }).on('error', function(e) {
-        console.error(e);
-        require('./server.js');
+        log.error(e);
+        loaded();
     });
+
+    function processUpdateInfo(err, info)
+    {
+        if (err) {
+            log.error('Failed to parse XML:', err);
+            loaded();
+            return false;
+        }
+
+        if (!info.updates) {
+            log.info('Up to date');
+            loaded();
+            return false;
+        }
+
+        var update = info.updates.update[0].patch[0].$;
+        update.size = parseInt(update.size, 10);
+
+        var crypto = require('crypto');
+
+        var zip = new Buffer(update.size);
+
+        var shasum = crypto.createHash(update.hashFunction);
+
+        https.get(update.URL, function(res) {
+
+            log.info('Downloading update');
+
+            var downloadedBytes = 0;
+
+            res.on('data', function(d) {
+                shasum.update(d);
+                d.copy(zip, downloadedBytes);
+                downloadedBytes += d.length;
+            });
+
+            res.on('end', function() {
+
+                if (downloadedBytes !== update.size) {
+                    log.error('Download incomplete');
+                    loaded();
+                    return;
+                }
+
+                log.info('Download complete');
+
+                var checksum = shasum.digest('hex');
+
+                if (checksum !== update.hashValue) {
+                    log.error('Checksum mismatch');
+                    loaded();
+                    return;
+                }
+
+                log.info('Applying update');
+
+                var AdmZip = require('adm-zip');
+
+                var archive = new AdmZip(zip);
+
+                archive.extractAllTo('.', true);
+
+                log.info('Update complete');
+
+                loaded();
+            });
+
+        }).on('error', function(e) {
+            log.error(e);
+            loaded();
+        });
+    }
+
+    function loaded()
+    {
+        require('./main.js')(log);
+        if (cb) cb();
+    }
+};
+
+if (!module.parent) {
+    var log = require('./logger.js')({loglevel: 'info', nocolor: false});
+    module.exports(log);
 }
 
-process.on('uncaughtException', function (err) {
-    console.log('uncaughtException:' + err);
-    console.log(err.stack);
-});
