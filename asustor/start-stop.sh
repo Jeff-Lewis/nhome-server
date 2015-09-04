098cec6b41fa647c3d7c2bf1890f351afe971518
Tom Atkinson <atkinson.tommy@nhome.ba>
2015-09-04 12:31:27 +0000

    Add support for Asustor NAS
    git-svn-id: svn://internal-repo/nsc/NHome/homeserver@126890 bc4cc02b-413e-4181-b44e-24092dc36e11

---
+++ asustor/start-stop.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+start_package()
+{
+    HOME=${APKG_PKG_DIR} /usr/local/bin/node ${APKG_PKG_DIR}/server.js --nocolor &> ${APKG_PKG_DIR}/server.log &
+}
+
+stop_package()
+{
+    /usr/bin/pkill -f server.js
+}
+
+case $1 in
+	start)
+		start_package
+        exit 0
+        ;;
+    stop)
+        stop_package
+        exit 0
+        ;;
+esac
