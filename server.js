bd080bd5caffcfe407d3f722cb345626b06e0600
Tom Atkinson <atkinson.tommy@nhome.ba>
2016-01-05 13:05:49 +0000

    Add support for setting a pidfile
    git-svn-id: svn://internal-repo/nsc/NHome/homeserver@133155 bc4cc02b-413e-4181-b44e-24092dc36e11

--- server.js
+++ server.js
@@ -12,9 +12,21 @@ program
   .version(getVersion())
   .option('-l, --loglevel [level]', 'Log level (fatal, error, warn, info, debug, trace) [info]', 'info')
   .option('--nocolor', 'Disable colors in terminal output', false)
+  .option('--pidfile [path]', 'Save a pid file at the given path', '')
   .parse(process.argv);
 
 var log = require('./lib/logger.js')(program, process.stdout);
+
+if (program.pidfile) {
+
+    var fs = require('fs');
+
+    fs.writeFile(program.pidfile, process.pid, function (err) {
+        if (err) {
+            logger.error('Failed to write pidfile', err);
+        }
+    });
+}
 
 require('./lib/main.js')(log);
 
